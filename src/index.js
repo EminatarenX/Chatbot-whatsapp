de3a11c7dc1e56d0fa82bcdd27e7cba251cfb5cd
EminatarenX <eminataren2002@gmail.com>
2023-08-16 13:21:15 -0600

    anadiendo la opcion --no-sandbox a puppeter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ const qrcode = require('qrcode-terminal');
 const fs = require('fs')
 
 const { Client, MessageMedia } = require('whatsapp-web.js');
-const client = new Client();
+const client = new Client({
+    puppeteer: {
+        args: ['--no-sandbox']
+    },
+    
+});
 
 client.on('qr', qr => {
     qrcode.generate(qr, {small: true});
